a1d40a865f9bc0ec094510fd01e892fa0f0c7fed
Lovepreet <lovepreetkaul23@gmail.com>
2021-01-24 14:19:21 +0530

    Implemented disabling canonical mode.

--- kilo.c
+++ kilo.c
@@ -1,12 +1,21 @@
 #include <termios.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 void enableRawMode() {
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
 
-    tcgetattr(STDIN_FILENO, &raw);
+    struct termios raw = orig_termios;
 
-    raw.c_lflag &= ~(ECHO);
+    raw.c_lflag &= ~(ECHO | ICANON);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
